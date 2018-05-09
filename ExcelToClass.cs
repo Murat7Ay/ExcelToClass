4b6855d4456102ff7a055a91d7fe47d6ad26014c
Murat Ay <murataybtp@gmail.com>
2018-05-10 00:03:32 +0300

    Excel sheet to class

---
+++ ExcelToClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+
+namespace ConsoleApplication7
+{
+    public class ExcelToClass
+    {
+        private string SheetName { get; set; }
+        private string ConnectionString { get; set; }
+        private DataTable dataTable { get; set; }
+        /// <summary>
+        /// Constructor to set  xlsx file path and the sheet name to fill your class model
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="sheetName"></param>
+        public ExcelToClass(string filePath,string sheetName)
+        {
+            ConnectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={filePath}; Extended Properties=Excel 12.0;";
+            SheetName = sheetName;
+        }
+
+        public void ReadFromExcel()
+        {
+            var adapter = new OleDbDataAdapter($"SELECT * FROM [{SheetName}$]", ConnectionString);
+            var ds = new DataSet();
+            adapter.Fill(ds, SheetName);
+            dataTable = ds.Tables[SheetName];
+        }
+
+        public List<T> LoadFromDataTable<T>() where T : class, new()
+        {
+            List<T> output = new List<T>();
+            T entry = new T();
+            var cols = entry.GetType().GetProperties();
+            var headers = dataTable.Columns.Cast<DataColumn>().Select(s => s.ColumnName).ToArray();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                entry = new T();
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    foreach(var col in cols)
+                    {
+                        if (col.Name == headers[i])
+                        {
+                            col.SetValue(entry, Convert.ChangeType(row[i], col.PropertyType));
+                        }
+                    }
+                }
+                output.Add(entry);
+            }
+            
+            return output;
+
+        }
+
+    }
+}
---
+++ SmartModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApplication7
+{
+    public class SmartModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CompanyName { get; set; }
+        public string CustomerNo { get; set; }
+        public string GSMNo { get; set; }
+        public string TcNo { get; set; }
+        public string SpecialField1 { get; set; }
+        public string SpecialField2 { get; set; }
+        public string SpecialField3 { get; set; }
+        public string SpecialField4 { get; set; }
+        public string SpecialField5 { get; set; }
+        public string AddressText { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public DateTime FirstDate { get; set; }
+        public DateTime LastDate { get; set; }
+        public string Spec1 { get; set; }
+        public string Spec2 { get; set; }
+        public string Spec3 { get; set; }
+        public string Spec4 { get; set; }
+        public string Spec5 { get; set; }
+        public string Spec6 { get; set; }
+        public string Spec7 { get; set; }
+        public string Spec8 { get; set; }
+        public string Spec9 { get; set; }
+        public string Spec10 { get; set; }
+        public string Spec11 { get; set; }
+        public string Spec12 { get; set; }
+        public string Spec13 { get; set; }
+        public string Spec14 { get; set; }
+        public string Spec15 { get; set; }
+        public string Spec16 { get; set; }
+        public string Spec17 { get; set; }
+        public string Spec18 { get; set; }
+        public string Spec19 { get; set; }
+        public string Spec20 { get; set; }
+        public string Spec21 { get; set; }
+        public string Spec22 { get; set; }
+        public string Spec23 { get; set; }
+        public string Spec24 { get; set; }
+        public string Spec25 { get; set; }
+        public string Spec26 { get; set; }
+        public string Spec27 { get; set; }
+        public string Spec28 { get; set; }
+        public string Spec29 { get; set; }
+        public string Spec30 { get; set; }
+        public string Spec31 { get; set; }
+        public string Spec32 { get; set; }
+        public string Spec33 { get; set; }
+        public string Spec34 { get; set; }
+        public string Spec35 { get; set; }
+        public string Spec36 { get; set; }
+        public string Spec37 { get; set; }
+        public string Spec38 { get; set; }
+        public string Spec39 { get; set; }
+        public string Spec40 { get; set; }
+        public string Spec41 { get; set; }
+        public string Spec42 { get; set; }
+        public string Spec43 { get; set; }
+        public string Spec44 { get; set; }
+        public string Spec45 { get; set; }
+        public string Spec46 { get; set; }
+        public string Spec47 { get; set; }
+        public string Spec48 { get; set; }
+        public string Spec49 { get; set; }
+        public string Spec50 { get; set; }
+    }
+}
